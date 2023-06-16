94a32775423a96ccef51ef4554fce8abf69f7726
kodanny0922 <136782301+kodanny0922@users.noreply.github.com>
2023-06-16 17:20:16 +1200

    Add files via upload

---
+++ app.py
@@ -0,0 +1,45 @@
+import pathlib
+pathlib.Path.home().joinpath(),'/Users/dannyko/PycharmProjects/Hello World'
+print(pathlib.Path.cwd()) #current directory
+
+
+
+'''
+🥶Ptahlib()
+Path.home()
+path.cwd
+/
+     Absolute path
+         eg.
+            PC # c\porgram Files\ Microsoft
+            Mac # /user/local/bin
+     Relative path
+     path()
+     mkdir()
+     rmdir()
+     eg.
+        path=path()
+        print(path.glob('*.*')
+        print(path.glob('*.py')
+        for file in path.glob('*.py'):
+            print(file)
+
+🥶Package:
+    Under a package directory
+    __init__.py
+
+
+        
+
+     🥰 ~~~Call all Moulds~~~
+    import ecommerce
+    ecommerce.shipping.calc_shipping()
+    🥰~~~Call all Moulds, object~~~
+    from ecommerce import shipping
+    shipping.calc_shipping()
+    🥰~~~Call all Moulds, Object, function~~~
+    from ecommerce.shipping import calc_shipping
+    calc_shipping()
+'''
+# from ecommerce import shipping
+# shipping.calc_shipping()
